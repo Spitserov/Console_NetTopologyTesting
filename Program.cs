bf960bee0da266d21607b3244ab1ac87e1e61405
ilia spitserov <iliaspitserov@gmail.com>
2023-08-21 10:39:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Console_NetTopologyTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select Command: " +
+                "\r\n1 = Measure the distance between points" +
+                "\r\n2 = Create a point..." +
+                "\r\n3 = Etc...");
+
+            string myCommandInput = Console.ReadLine();
+
+            switch (myCommandInput)
+            {
+                case "1":
+                    Console.WriteLine("Distance between points is: " + TestingUtils.MeasureDistanceBetweenPoints());
+                    Console.ReadLine();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Your NetTopologySuite Point is: " + TestingUtils.CreatePoint());
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    break;
+            }
+
+
+
+            //
+
+            //Console.WriteLine("you selected a different command");
+
+
+        }
+    }
+}
---
+++ TestingUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//don't have to call for NetTopologySuite library 
+
+namespace Console_NetTopologyTesting
+{
+    internal class TestingUtils
+    {
+
+        public static NetTopologySuite.Geometries.Point CreatePoint()
+        {
+            double xInput = Convert.ToDouble(Console.ReadLine());
+            double yInput = Convert.ToDouble(Console.ReadLine());
+            return new NetTopologySuite.Geometries.Point(xInput, yInput);
+        }
+
+        public static double MeasureDistanceBetweenPoints()
+        {
+            Console.WriteLine("Insert Point1 X,Y values");
+            NetTopologySuite.Geometries.Point myPoint1 = CreatePoint();
+            Console.WriteLine("Insert Point2 X,Y values");
+            NetTopologySuite.Geometries.Point myPoint2 = CreatePoint();
+
+            return myPoint1.Distance(myPoint2);
+        }
+    }
+}
